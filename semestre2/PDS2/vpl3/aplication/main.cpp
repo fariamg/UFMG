8a5eaabc3ccae92fa1df1010c386e235ffadb687
Gabriel <fariamg4@gmail.com>
2024-11-05 13:45:08 -0300

    Melhorias vpl3

--- semestre2/PDS2/vpl3/aplication/main.cpp
+++ semestre2/PDS2/vpl3/aplication/main.cpp
@@ -17,30 +17,33 @@ int main() {
             
             Onibus* onibus = new Onibus(placa, capacidade);
 
-            if (empresa->adicionar_onibus(onibus) == nullptr) {
+            if (!empresa->adicionar_onibus(onibus)) {
                 cout << "ERRO: onibus repetido" << endl;
                 delete onibus;
-            } else {
-                cout << "novo onibus cadastrado" << endl;
+                continue;
             }
+            
+            cout << "novo onibus cadastrado" << endl;
         }
-        
+
         if (entrada == 'S') {
             string placa;
             int num_pessoas;
             cin >> placa >> num_pessoas;
 
             Onibus* onibus = empresa->busca_onibus(placa);
-            if (onibus != nullptr) {
-                if (onibus->lotacao_atual + num_pessoas <= onibus->capacidade_maxima) {
-                    onibus->subir_passageiros(num_pessoas);
-                    cout << "passageiros subiram com sucesso" << endl;
-                } else {
-                    cout << "ERRO: onibus lotado" << endl;
-                }
-            } else {
-                cout << "ERRO: inexistente" << endl;
+
+            if (!onibus) {
+                cout << "ERRO: onibus inexistente" << endl;
+                continue;
             }
+
+            if (onibus->lotacao_atual + num_pessoas > onibus->capacidade_maxima) 
+                cout << "ERRO: onibus lotado" << endl;
+                continue;
+            
+            onibus->subir_passageiros(num_pessoas);
+            cout << "passageiros subiram com sucesso" << endl;
         }
 
         if (entrada == 'D') {
@@ -49,16 +52,19 @@ int main() {
             cin >> placa >> num_pessoas;
 
             Onibus* onibus = empresa->busca_onibus(placa);
-            if (onibus != nullptr) {
-                if (onibus->lotacao_atual - num_pessoas >= 0) {
-                    onibus->descer_passageiros(num_pessoas);
-                    cout << "passageiros desceram com sucesso" << endl;
-                } else {
-                    cout << "ERRO: faltam passageiros" << endl;
-                }
-            } else {
+
+            if (!onibus) {
                 cout << "ERRO: onibus inexistente" << endl;
+                continue;
             }
+
+            if (onibus->lotacao_atual - num_pessoas < 0) {
+                cout << "ERRO: faltam passageiros" << endl;
+                continue;
+            }
+
+            onibus->descer_passageiros(num_pessoas);
+            cout << "passageiros desceram com sucesso" << endl;
         }
 
         if (entrada == 'T') {
@@ -68,16 +74,19 @@ int main() {
 
             Onibus* onibus1 = empresa->busca_onibus(placa1);
             Onibus* onibus2 = empresa->busca_onibus(placa2);
-            if (onibus1 != nullptr && onibus2 != nullptr) {
-                if (onibus1->lotacao_atual - num_pessoas >= 0 && onibus2->lotacao_atual + num_pessoas <= onibus2->capacidade_maxima) {
-                    onibus1->transfere_passageiros(onibus2, num_pessoas);
-                    cout << "transferencia de passageiros efetuada" << endl;
-                } else {
-                    cout << "ERRO: transferencia cancelada" << endl;
-                }
-            } else {
+
+            if (!onibus1 || !onibus2) {
                 cout << "ERRO: onibus inexistente" << endl;
+                continue;
             }
+
+            if (onibus1->lotacao_atual - num_pessoas < 0 || onibus2->lotacao_atual + num_pessoas > onibus2->capacidade_maxima) {
+                cout << "ERRO: transferencia cancelada" << endl;
+                continue;
+            }
+
+            onibus1->transfere_passageiros(onibus2, num_pessoas);
+            cout << "transferencia de passageiros efetuada" << endl;
         }
 
         if (entrada == 'I') {
